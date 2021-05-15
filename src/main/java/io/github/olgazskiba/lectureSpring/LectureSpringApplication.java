89faff66fba38e4427eec0c68c15bfaf1cf967ac
olgaZskiba <73237906+olgaZskiba@users.noreply.github.com>
2021-05-15 16:09:11 +0300

    add start.spring.io project

---
+++ src/main/java/io/github/olgazskiba/lectureSpring/LectureSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.olgazskiba.lectureSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/olgazskiba/lectureSpring/LectureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.olgazskiba.lectureSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
